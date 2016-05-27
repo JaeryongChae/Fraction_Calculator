39d1673cd94e1512948ee898061473a3b3354ea7
Jaeryong Chae <jr_chae@naver.com>
2016-05-27 14:44:19 +0900

    Initial Commit

---
+++ Fraction_Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fraction_Calculator
+//
+//  Created by MF839-008 on 2016. 5. 27..
+//  Copyright © 2016년 JRChae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fraction_Calculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fraction_Calculator
+//
+//  Created by MF839-008 on 2016. 5. 27..
+//  Copyright © 2016년 JRChae. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
